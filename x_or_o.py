1a82c023ada7c5f891574f8f350e94f52c5af3d2
mikhail-batanov <404.mikhail@gmail.com>
2019-09-04 11:20:16 +0300

    Update x_or_o.py
    Add classes, attributes and methods

--- x_or_o.py
+++ x_or_o.py
@@ -1,117 +1,160 @@
-class color:
-    Red = '\033[91m'
-    Green = '\033[92m'
-    END = '\033[0m'
+class field:
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    win_coordinates = [[0, 1, 2], [0, 4, 8], [0, 3, 6],
+                       [1, 4, 7], [2, 5, 8], [2, 4, 6],
+                       [3, 4, 5], [6, 7, 8]]
+    X = '\033[92m' + 'X' + '\033[0m'
+    O = '\033[91m' + 'O' + '\033[0m'
+    # '\033[92m' - зеленый цвет
+    # '\033[91m' - красный цвет
+    # '\033[0m' - конец цвета
 
-def revect():
-    return [[s[0], s[1], s[2]], [s[0], s[4], s[8]], [s[0], s[3], s[6]],
-            [s[1], s[4], s[7]], [s[2], s[5], s[8]], [s[2], s[4], s[6]],
-            [s[3], s[4], s[5]], [s[6], s[7], s[8]]]
+    def win_vectors(self):
+        return [[field.board[0], field.board[1], field.board[2]],
+                [field.board[0], field.board[4], field.board[8]],
+                [field.board[0], field.board[3], field.board[6]],
+                [field.board[1], field.board[4], field.board[7]],
+                [field.board[2], field.board[5], field.board[8]],
+                [field.board[2], field.board[4], field.board[6]],
+                [field.board[3], field.board[4], field.board[5]],
+                [field.board[6], field.board[7], field.board[8]]]
+
+    def board_output(self):
+        i = 0
+        while i < 8:
+            print(field.board[i], field.board[i + 1], field.board[i + 2])
+            i = i + 3
 
 
-def pprint():
-    i = 0
-    while i < 8:
-        print (s[i], s[i+1], s[i+2])
-        i = i + 3
-
-
-def check():
-    vect = revect()
-    j = 0
-    while j < len(vect):
-        if vect[j].count(X) == 3 or vect[j].count(O) == 3:
-            return 1
-            break
+class check:
+    def win(self):
+        vect = field.win_vectors(0)
+        j = 0
+        while j < len(vect):
+            if vect[j].count(field.X) == 3 or vect[j].count(field.O) == 3:
+                if vect[j].count(field.X) == 3:
+                    print('Крестики выиграли!')
+                if vect[j].count(field.O) == 3:
+                    print('Нолики выиграли!')
+                return True
+            else:
+                j = j + 1
         else:
+            return False
+
+    def draw(self):
+        while 1 == 1:
+            if not any(e in range(len(field.board) + 1) for e in field.board):
+                return True
+            else:
+                return False
+
+    def move(self):
+        number = self
+        if number.isdigit():
+            number = int(number)
+            if number >= 1 and number <= 9:
+                if field.board[number - 1] != field.X\
+                        and field.board[number - 1] != field.O:
+                    return True
+        else:
+            return False
+
+
+class computer:
+    def step(self):
+        j, k = 0, 0
+        pars = field.win_vectors(0)
+        cs = 0
+        while j < len(pars):
+            if pars[j].count(field.X) < 1 \
+                    and pars[j].count(field.O) > 1 and not cs:
+                while k < len(pars[j]):
+                    if pars[j][k] != field.O:
+                        field.board[field.win_coordinates[j][k]] = field.O
+                        cs = 1
+                        break
+                    k = k + 1
             j = j + 1
+        j, k = 0, 0
+        while j < len(pars):
+            if pars[j].count(field.X) > 1 \
+                    and pars[j].count(field.O) < 1 and not cs:
+                while k < len(pars[j]):
+                    if pars[j][k] != field.X:
+                        field.board[field.win_coordinates[j][k]] = field.O
+                        cs = 1
+                        break
+                    k = k + 1
+            j = j + 1
+        if field.board[0] == field.X and field.board[8] == field.X \
+                and field.board[1] != field.O \
+                and field.board[1] != field.X and not cs:
+            field.board[1] = field.O
+            cs = 1
+        if field.board[2] == field.X and field.board[6] == field.X \
+                and field.board[7] != field.O \
+                and field.board[7] != field.X and not cs:
+            field.board[7] = field.O
+            cs = 1
+        if field.board[4] == field.O or field.board[4] == field.X and not cs:
+            if field.board[0] != field.X \
+                    and field.board[0] != field.O and not cs:
+                field.board[0] = field.O
+                cs = 1
+            if field.board[2] != field.X \
+                    and field.board[2] != field.O and not cs:
+                field.board[2] = field.O
+                cs = 1
+            if field.board[6] != field.X \
+                    and field.board[6] != field.O and not cs:
+                field.board[6] = field.O
+                cs = 1
+            if field.board[8] != field.X \
+                    and field.board[8] != field.O and not cs:
+                field.board[8] = field.O
+                cs = 1
+        if field.board[4] != field.O \
+                and field.board[4] != field.X and not cs:
+            field.board[4] = field.O
+            cs = 1
+        while k < len(field.board):
+            # ход на первую свободную ячейку по порядковому номеру
+            if field.board[k] != field.X \
+                    and field.board[k] != field.O and not cs:
+                field.board[k] = field.O
+                cs = 1
+                break
+            k = k + 1
+        print('Ход ноликов:')
+        field.board_output(0)
 
 
-def step():
-    while 1 == 1:
-        if not any(e in range(len(s)+1) for e in s):
-            print('Ничья')
-            break
-        ops = 0
-        while not ops:
+class player:
+    def step(self):
+        while 1 == 1:
+            if check.draw(0):
+                print('Ничья.')
+                break
             number = input(
                 'Ход крестиков, укажите доступную цифру для хода: ', )
-            if number.isdigit():
-                number = int(number)
-                if number >= 1 and number <= 9:
-                    if s[number-1] != X and s[number-1] != O:
-                        ops = 1
-        s[number-1] = X
-        if check():
-            pprint()
-            print('Крестики выиграли!')
-            break
-        comp_step()
-        if check():
-            print('Нолики выиграли!')
-            break
+            while not check.move(number):
+                number = input(
+                    'Ход крестиков, укажите доступную цифру для хода: ', )
+            number = int(number)
+            field.board[number - 1] = field.X
+            if check.draw(0):
+                print('Ничья.')
+                field.board_output(0)
+                break
+            if check.win(0):
+                field.board_output(0)
+                break
+            computer.step(0)
+            if check.win(0):
+                break
 
 
-def comp_step():
-    j, k = 0, 0
-    pars = revect()
-    cs = 0
-    while j < len(pars):
-        if pars[j].count(X) < 1 and pars[j].count(O) > 1 and not cs:
-            while k < len(pars[j]):
-                if pars[j][k] != O:
-                    s[omni[j][k]] = O
-                    cs = 1
-                    break
-                k = k + 1
-        j = j + 1
-    j, k = 0, 0
-    while j < len(pars):
-        if pars[j].count(X) > 1 and pars[j].count(O) < 1 and not cs:
-            while k < len(pars[j]):
-                if pars[j][k] != X:
-                    s[omni[j][k]] = O
-                    cs = 1
-                    break
-                k = k + 1
-        j = j + 1
-    if s[0] == X and s[8] == X and s[1] != O and s[1] != X and not cs:
-        s[1] = O
-        cs = 1
-    if s[2] == X and s[6] == X and s[7] != O and s[7] != X and not cs:
-        s[7] = O
-        cs = 1
-    if s[4] == O or s[4] == X and not cs:
-        if s[0] != X and s[0] != O and not cs:
-            s[0] = O
-            cs = 1
-        if s[2] != X and s[2] != O and not cs:
-            s[2] = O
-            cs = 1
-        if s[6] != X and s[6] != O and not cs:
-            s[6] = O
-            cs = 1
-        if s[8] != X and s[8] != O and not cs:
-            s[8] = O
-            cs = 1
-    if s[4] != O and s[4] != X and not cs:
-        s[4] = O
-        cs = 1
-    while k < len(s):
-        if s[k] != X and s[k] != O and not cs:
-            s[k] = O
-            cs = 1
-            break
-        k = k + 1
-    print('Ход ноликов:')
-    pprint()
-
-s = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-omni = [[0, 1, 2], [0, 4, 8], [0, 3, 6],
-        [1, 4, 7], [2, 5, 8], [2, 4, 6],
-        [3, 4, 5], [6, 7, 8]]
-X = color.Green + 'X' + color.END
-O = color.Red + 'O' + color.END
-print('Это поле для игры:')
-pprint()
-step()
+print ('Это поле для игры:')
+field.board_output(0)
+player.step(0)
