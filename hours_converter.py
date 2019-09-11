0a14c6ebdd64eccf676de16c88896fb35fc71369
mikhail-batanov <404.mikhail@gmail.com>
2019-09-11 11:39:00 +0300

    Create hours_converter.py
    Решение задачи тайм конвертер на пайчекио

---
+++ hours_converter.py
@@ -0,0 +1,16 @@
+# Решение задачи тайм конвертер на пайчекио
+# Автор - M. Batanov, 11.09.19
+
+
+def time_converter(time):
+    hours = int(time[0] + time[1])
+    result = time[2:5]
+    if hours > 11:
+        if hours != 12:
+            hours = hours - 12
+        result = str(hours) + result + ' p.m.'
+    else:
+        if hours == 0:
+            hours = 12
+        result = str(hours) + result + ' a.m.'
+    return result
