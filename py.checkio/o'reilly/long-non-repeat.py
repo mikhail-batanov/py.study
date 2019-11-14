d723f36334cc224779687e67d7b6bf4ecfdac44d
mikhail-batanov <404.mikhail@gmail.com>
2019-11-14 12:42:10 +0300

    Update long-non-repeat.py

--- py.checkio/o'reilly/long-non-repeat.py
+++ py.checkio/o'reilly/long-non-repeat.py
@@ -1,4 +1,6 @@
-# Заготовка решения py.checkio.org/mission/long-non-repeat/solve/
+# Решение задачи  "Long Non Repeat" в пайчекио
+# Задача: py.checkio.org/mission/long-non-repeat/
+# Автор: M. Batanov, 14.11.19
 
 
 def non_repeat(line):
@@ -14,20 +16,10 @@ def non_repeat(line):
             if e == len(line) - 1:
                 array.append(temp_line)
     for i in range(len(array)):
-        for e in range(len(array)):
-            if len(array[i]) > len(array[e]):
+        for e in range(i, len(array)):
+            if len(array[i]) < len(array[e]):
                 array[i], array[e] = array[e], array[i]
-    print(array)
     if len(array) > 0:
         return array[0]
     else:
-        return array
-
-
-non_repeat('abdjwawk')
-non_repeat("abcabcffab")
-non_repeat("fghfrtyfgh")
-non_repeat("w")
-non_repeat("aaaaa")
-non_repeat("wq")
-non_repeat("fghfhy")
+        return ''
